7ec1744ab0bf3aea63d0ebb5ac0318298d91fc33
Oscar Omar Gonzalez Toledo <oscar.gonzalez@tautenet.com.mx>
2022-06-17 09:15:26 -0600

    JS4

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -17,10 +17,11 @@ function crearGato (nombre, edad) {
 }
 
 function agregarPropiedad (objeto, property) {
-  // Agrega una propiedad al objeto (argumento "objeto") con el valor `null`
+  // Agrega una propiedad al objeto (argumento"objeto") con el valor `null`
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+
   objeto[property] = null;
   return objeto;
 }
